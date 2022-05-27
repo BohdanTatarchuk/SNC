8a97504031c8f6ec3dff163925d09c7a076ff056
Bohdan Tatarchuk <102388122+BohdanTatarchuk@users.noreply.github.com>
2022-05-27 20:37:55 +0200

    Menu of 4 commands was added. Structure if else was changed to switch case.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -28,14 +28,49 @@ public class Bot extends TelegramLongPollingBot {
             System.out.println(update.getMessage().getText());
             System.out.println(update.getMessage().getChatId().toString());
 
-            //beginning
-            if(text.equals("/start")){
-                message.setText("Привет! Ты в боте APEX 1.0.\uD83D\uDC7E \n" +
-                        "\nДля начала выбери язык:");
-                try {
-                    execute(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
+            //default commands
+            final String about = """
+                    Space Noodles Corporations - это NFT — коллекция на блокчейне TON, которая позволяет каждому обзавестись уникальным космическим кораблём и стать частью уникального комьюнити. Выбирайте любой из 555 отрисованных вручную токенов Вселенной SNC.
+
+                    Помимо самого токена и возможности его перепродажи на вторичном рынке, вы получаете внушительный набор Плюшек. Подробнее на нашем сайте \uD83D\uDC7E""";
+            final String start = """
+                    Привет! Ты в боте APEX 1.0.\uD83D\uDC7E\s
+
+                    Для начала выбери язык:""";
+            final String instruction = "Здесь самая актуальная информация о TON. Ниже список доступных инструкций.";
+
+            switch (text) {
+                case "/start" -> {
+                    message.setText(start);
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case "/referal" -> {
+                    message.setText("*link*");
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case "/about" -> {
+                    message.setText(about);
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case "/instruction" -> {
+                    message.setText(instruction);
+                    try {
+                        execute(message);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
