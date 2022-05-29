ee6fb98ac195f52789979fc1876ad82d2965d826
Bohdan Tatarchuk <102388122+BohdanTatarchuk@users.noreply.github.com>
2022-05-29 13:45:10 +0200

    Buttons of language were added

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,5 +1,6 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -9,6 +10,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
+
+    final String about = """
+                    Space Noodles Corporations - это NFT — коллекция на блокчейне TON, которая позволяет каждому обзавестись уникальным космическим кораблём и стать частью уникального комьюнити. Выбирайте любой из 555 отрисованных вручную токенов Вселенной SNC.
+
+                    Помимо самого токена и возможности его перепродажи на вторичном рынке, вы получаете внушительный набор Плюшек. Подробнее на нашем сайте \uD83D\uDC7E""";
+    final String start = " Привет! Ты в боте APEX 1.0.\uD83D\uDC7E\s";
+    final String language = "Выбери язык:";
+    final String instruction = "Здесь самая актуальная информация о TON. Ниже список доступных инструкций.";
+    final String menu = "Ты в главном меню \uD83C\uDF9B";
+
 
     @Override
     public String getBotUsername() {
@@ -34,31 +45,35 @@ public class Bot extends TelegramLongPollingBot {
             System.out.println(update.getMessage().getChatId().toString());
 
             //default commands
-            final String about = """
-                    Space Noodles Corporations - это NFT — коллекция на блокчейне TON, которая позволяет каждому обзавестись уникальным космическим кораблём и стать частью уникального комьюнити. Выбирайте любой из 555 отрисованных вручную токенов Вселенной SNC.
-
-                    Помимо самого токена и возможности его перепродажи на вторичном рынке, вы получаете внушительный набор Плюшек. Подробнее на нашем сайте \uD83D\uDC7E""";
-            final String start = """
-                    Привет! Ты в боте APEX 1.0.\uD83D\uDC7E\s
-
-                    Для начала выбери язык:""";
-            final String instruction = "Здесь самая актуальная информация о TON. Ниже список доступных инструкций.";
-
                 switch (text) {
                 case "/start" -> {
-                    message.setText(start);
                     try {
+                        message.setText(start);
+                        execute(message);
                         execute(
                                 SendMessage.builder()
-                                        .text(start)
+                                        .text(language)
                                         .chatId(message.getChatId())
-                                        .replyMarkup(getMessagesButtons())
+                                        .replyMarkup(getLanguageButtons())
                                         .build()
                         );
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
                 }
+                case "/menu" -> {
+                        try {
+                            execute(
+                                    SendMessage.builder()
+                                            .text(menu)
+                                            .chatId(message.getChatId())
+                                            .replyMarkup(getMainMenuButtons())
+                                            .build()
+                            );
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 case "/referal" -> {
                     message.setText("*link*");
                     try {
@@ -68,17 +83,27 @@ public class Bot extends TelegramLongPollingBot {
                     }
                 }
                 case "/about" -> {
-                    message.setText(about);
                     try {
-                        execute(message);
+                        execute(
+                                SendMessage.builder()
+                                        .text(about)
+                                        .chatId(message.getChatId())
+                                        .replyMarkup(getAboutButtons())
+                                        .build()
+                        );
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
                 }
                 case "/instruction" -> {
-                    message.setText(instruction);
                     try {
-                        execute(message);
+                        execute(
+                                SendMessage.builder()
+                                        .text(instruction)
+                                        .chatId(message.getChatId())
+                                        .replyMarkup(getInstructionButton())
+                                        .build()
+                        );
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
@@ -88,27 +113,155 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     //buttons language
-    private InlineKeyboardMarkup getMessagesButtons(){
+    private InlineKeyboardMarkup getLanguageButtons(){
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
         InlineKeyboardButton ukr = new InlineKeyboardButton();
         ukr.setText("Українська\uD83C\uDDFA\uD83C\uDDE6");
+
         InlineKeyboardButton ru = new InlineKeyboardButton();
         ru.setText("Русский\uD83C\uDDF7\uD83C\uDDFA");
+
         //callback data
         ukr.setCallbackData("ukr");
         ru.setCallbackData("ru");
-        //list of buttons
-        List<InlineKeyboardButton> buttonList = new ArrayList<>();
-        buttonList.add(ukr);
-        buttonList.add(ru);
+
+        //lists of buttons
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        row1.add(ukr);
+        List<InlineKeyboardButton> row2 = new ArrayList<>();
+        row2.add(ru);
+
         //list of lists of buttons
         List< List<InlineKeyboardButton>> buttons = new ArrayList<>();
-        buttons.add(buttonList);
+        buttons.add(row1);
+        buttons.add(row2);
+
         //inline keyboard markup
         inlineKeyboardMarkup.setKeyboard(buttons);
-
         return inlineKeyboardMarkup;
     }
 
     //buttons main menu
+    private InlineKeyboardMarkup getMainMenuButtons(){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton instructions = new InlineKeyboardButton();
+        instructions.setText("ИНСТРУКЦИИ⚡");
+
+        InlineKeyboardButton aboutProject = new InlineKeyboardButton();
+        aboutProject.setText("О проекте\uD83D\uDC40");
+
+        InlineKeyboardButton tgChannel = new InlineKeyboardButton();
+        tgChannel.setText("Канал SNC\uD83D\uDC7E");
+        String link = "t.me.spacenoodles";
+        tgChannel.setUrl(link);
+
+        InlineKeyboardButton referalLink = new InlineKeyboardButton();
+        referalLink.setText("Рефер.Ссылка🔗");
+
+        //callback data
+        instructions.setCallbackData("instructions");
+        tgChannel.setCallbackData("channel");
+        aboutProject.setCallbackData("channel");
+        referalLink.setCallbackData("channel");
+
+        //lists of buttons
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        row1.add(tgChannel);
+        row1.add(instructions);
+
+        List<InlineKeyboardButton> row2 = new ArrayList<>();
+        row2.add(aboutProject);
+        row2.add(referalLink);
+
+        //list of lists of buttons
+        List< List<InlineKeyboardButton>> buttons = new ArrayList<>();
+        buttons.add(row1);
+        buttons.add(row2);
+
+        //inline keyboard markup
+        inlineKeyboardMarkup.setKeyboard(buttons);
+        return inlineKeyboardMarkup;
+    }
+
+    //buttons about buttons
+    private InlineKeyboardMarkup getAboutButtons(){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton back = new InlineKeyboardButton();
+        back.setText("Назад");
+        back.setCallbackData("back");
+
+        InlineKeyboardButton website = new InlineKeyboardButton();
+        website.setCallbackData("website");
+        String link = "https://spacenoodles.ru";
+        website.setUrl(link);
+        website.setText("Наш сайт");
+
+        //list of lists of buttons
+        List<InlineKeyboardButton> buttons = new ArrayList<>();
+        buttons.add(back);
+        buttons.add(website);
+
+        List<List<InlineKeyboardButton>> list = new ArrayList<>();
+        list.add(buttons);
+
+        //inline keyboard markup
+        inlineKeyboardMarkup.setKeyboard(list);
+        return inlineKeyboardMarkup;
+    }
+
+    //buttons instruction
+    private InlineKeyboardMarkup getInstructionButton(){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
+        InlineKeyboardButton back = new InlineKeyboardButton();
+        back.setText("Назад");
+        back.setCallbackData("back");
+
+        InlineKeyboardButton nft = new InlineKeyboardButton();
+        nft.setText("Ликбез по NFT");
+        nft.setCallbackData("nft");
+        String linkNFT = "https://telegra.ph/Likbez-po-NFT-05-26";
+        nft.setUrl(linkNFT);
+
+        InlineKeyboardButton wallet = new InlineKeyboardButton();
+        wallet.setText("Кошелек TON");
+        wallet.setCallbackData("wallet");
+        String linkWallet = "https://telegra.ph/Koshelek-TON-05-26";
+        wallet.setUrl(linkWallet);
+
+        InlineKeyboardButton termin = new InlineKeyboardButton();
+        termin.setText("Терминология");
+        termin.setCallbackData("termin");
+        String linkTermin = "https://telegra.ph/Terminologiya-05-26";
+        termin.setUrl(linkTermin);
+
+        InlineKeyboardButton safety = new InlineKeyboardButton();
+        safety.setText("Безопасность");
+        safety.setCallbackData("safety");
+        String linkSafety = "https://telegra.ph/Bezopasnost-05-26-2";
+        safety.setUrl(linkSafety);
+
+        //list
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        row1.add(nft);
+        row1.add(termin);
+        List<InlineKeyboardButton> row2 = new ArrayList<>();
+        row2.add(wallet);
+        row2.add(safety);
+        List<InlineKeyboardButton> row3 = new ArrayList<>();
+        row3.add(back);
+
+        //list of lists
+        List<List<InlineKeyboardButton>> list = new ArrayList<>();
+        list.add(row1);
+        list.add(row2);
+        list.add(row3);
+
+        //inline keyboard markup
+        inlineKeyboardMarkup.setKeyboard(list);
+        return inlineKeyboardMarkup;
+    }
 }
